69f65162847b9acdacb4353f04879d32fdb79a30
SashaDmykhovskyi <sashadmykhovski@gmail.com>
2026-01-30 08:33:39 +0200

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Хмелевський Владіслав on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
